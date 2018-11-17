e69967bd8e87475041f1df9d6e34247729bace5b
Launchpaddy <trevikp@gmail.com>
2018-11-17 16:44:32 -0700

    no idea

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ app.get("/", function(req, res) {
    res.end();
 });
 
-// app.listen(5000, function() {
-//     console.log("Listening on port 5000");
-// });
 
 app.get("/getRate", function(req, res) {
    // Controller
@@ -88,13 +85,3 @@ function getPrice(method, weight) {
 
    return price.toFixed(2);
 }
-// const express = require('express');
-// const path = require('path');
-// const PORT = process.env.PORT || 5000;
-// var app = express();
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  // .get('/', (req, res) => res.render('pages/index'))
